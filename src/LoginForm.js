b6cc0cc3630685eb99e4b746e082e517aabccba6
aribfp <aribpelayo@gmail.com>
2023-12-13 12:38:01 +1300

    updated environment due to old version not compatible

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -25,18 +25,18 @@ export default function LoginForm() {
     <div className="wrapper">
       <div className="row">
         <label htmlFor={"email"}>Email</label>
-        <input id={"email"} type={"email"}  />
+        <input id={"email"} type={"email"} onChange={(e) => setEmail(email)}  />
       </div>
       <div className="row">
         <label htmlFor={"password"}>Password</label>
-        <input id={"password"} type={"password"}/>
+        <input id={"password"} type={"password"}  onChange={(e) => setPassword(password)}/>
       </div>
 
       {/* Place login error inside this div. Show the div ONLY if there are login errors. */}
       <div className="errorMessage"></div>
 
       <div className="button">
-        <button>Login</button>
+        <button> Login</button>
       </div>
     </div>
   );
